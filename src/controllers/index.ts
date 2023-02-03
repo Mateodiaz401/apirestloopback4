1c2b8866f73de6ad0355aea0265241a60e0e64e2
Mateo Díaz <mateodiaz401@gmail.com>
2023-02-03 12:55:33 -0500

    fir commit

---
+++ src/controllers/index.ts
@@ -0,0 +1,6 @@
+export * from './ping.controller';
+export * from './todo.controller';
+export * from './todo-list-todo.controller';
+export * from './todo-todo-list.controller';
+export * from './todolist.controller';
+export * from './user.controller';
---
+++ src/repositories/todo-list.repository.ts
@@ -0,0 +1,25 @@
+import {inject, Getter} from '@loopback/core';
+import {DefaultCrudRepository, repository, HasManyRepositoryFactory} from '@loopback/repository';
+import {DbDataSource} from '../datasources';
+import {TodoList, TodoListRelations, Todo} from '../models';
+import {TodoRepository} from './todo.repository';
+
+export class TodoListRepository extends DefaultCrudRepository<
+  TodoList,
+  typeof TodoList.prototype.id,
+  TodoListRelations
+> {
+
+  public readonly todos: HasManyRepositoryFactory<Todo, typeof TodoList.prototype.id>;
+
+  constructor(
+    @inject('datasources.db') dataSource: DbDataSource, @repository.getter('TodoRepository') protected todoRepositoryGetter: Getter<TodoRepository>,
+  ) {
+    super(TodoList, dataSource);
+    this.todos = this.createHasManyRepositoryFactoryFor('todos', todoRepositoryGetter,);
+    this.registerInclusionResolver('todos', this.todos.inclusionResolver);
+  }
+  public findByTitle(title: string) {
+    return this.findOne({where: {title}})
+  }
+}
